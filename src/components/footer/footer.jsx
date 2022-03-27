cca9340b8bf99cb486a1fc00aba63dd723f911b6
Tran Anh (e1900305) <namanh022@gmail.com>
2022-03-27 20:35:57 +0300

    add footer component

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import './footer.css'
-const footer = () => {
+import {AiFillInstagram,AiFillFacebook} from'react-icons/ai'
+const Footer = () => {
   return (
-    <div>footer</div>
+    <footer>
+      <a href="#header" className='footer__logo'>Tran Anh</a>
+
+      <ul className='permalinks'>
+        <li><a href="#header">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://www.facebook.com/namanh.tranvan.9/"><AiFillFacebook/></a>
+        <a href="https://www.instagram.com/namanh022/"><AiFillInstagram/></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Tran Anh</small>
+      </div>
+    </footer>
   )
 }
 
-export default footer
+export default Footer
